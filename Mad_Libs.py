deae291bdcb537eef193432acd4a47eaaf60039c
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2019-09-02 23:41:34 -0700

    Fixed list structure

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -17,98 +17,57 @@ class colors:
         pink='\033[95m'
         lightcyan='\033[96m'
 #pronouns list
-pronouns_list = []
 
-first_pronoun = input(colors.red +'Type your first and last name: ')
-second_pronoun = input('Type your city and state: ')
-third_pronoun = input('Type a friends name: ')
-fourth_pronoun = input('Type another friend\'s name: ')
+pronouns_list = ['pronoun', 'pronoun', 'pronoun', 'pronoun']
 
-pronouns_list.append(first_pronoun)
-pronouns_list.append(second_pronoun)
-pronouns_list.append(third_pronoun)
-pronouns_list.append(fourth_pronoun)
+pronouns_list[0] = input(colors.red +'Type your first and last name: ')
+pronouns_list[1] = input(colors.red +'Type your city and state: ')
+pronouns_list[2] = input(colors.red +'Type a friends name: ')
+pronouns_list[3] = input(colors.red +'Type another friend\'s name: ')
+
 #nouns list
-nouns_list = []
+nouns_list = ['noun','noun','noun','noun','noun','noun','noun','noun']
 
-first_noun = input('Type a noun: ')
-second_noun = input('Type a group of people: ')
-third_noun = input('Type a relationship i.e. brother: ')
-fourth_noun = input('Type a noun: ')
-fifth_noun = input('Type a body part: ')
-sixth_noun = input('Type a food item: ')
-seventh_noun = input('Type a popular chain restaurant: ')
-eighth_noun = input('Type an noun: ')
-
-
-
-nouns_list.append(first_noun)
-nouns_list.append(second_noun)
-
-nouns_list.append(third_noun)
-nouns_list.append(fourth_noun)
-nouns_list.append(fifth_noun)
-nouns_list.append(sixth_noun)
-nouns_list.append(seventh_noun)
-nouns_list.append(eighth_noun)
-
+nouns_list[0] = input(colors.pink+ 'Type a noun: ')
+nouns_list[1] = input(colors.pink+'Type a group of people: ')
+nouns_list[2] = input(colors.pink+'Type a relationship i.e. brother: ')
+nouns_list[3] = input(colors.pink+'Type a noun: ')
+nouns_list[4] = input(colors.pink+'Type a body part: ')
+nouns_list[5] = input(colors.pink+'Type a food item: ')
+nouns_list[6]= input(colors.pink+'Type a popular chain restaurant: ')
+nouns_list[7] = input(colors.pink+'Type an noun: ')
 
 #verb list
-verb_list = []
+verb_list = ['verb','verb','verb','verb','verb']
 
-first_verb = input('Type a verb, past tense: ')
-second_verb = input('Type a verb (ing): ')
-third_verb = input('Type a verb, present tense: ')
-fourth_verb = input('Type a verb, past tense: ')
-fifth_verb = input('Type a verb (ing): ')
-
-verb_list.append(first_verb)
-verb_list.append(second_verb)
-verb_list.append(third_verb)
-verb_list.append(fourth_verb)
-verb_list.append(fifth_verb)
+verb_list[0] = input(colors.blue+'Type a verb, past tense: ')
+verb_list[1]= input(colors.blue+ 'Type a verb (ing): ')
+verb_list[2] = input(colors.blue+ 'Type a verb, present tense: ')
+verb_list[3] = input(colors.blue+ 'Type a verb, past tense: ')
+verb_list[4] = input(colors.blue+ 'Type a verb (ing): ')
 
 #adjectives list
-adj_list = []
+adj_list = ['adjective','adjective','adjective','adjective','adjective','adjective']
 
-first_adj = input('Type an physical adjective: ')
-second_adj = input('Type an emotional adjective: ')
-third_adj = input('Type an physical adjective: ')
-fourth_adj = input('Type an physical adjective: ')
-fifth_adj = input('Type an emotional adjective: ')
-sixth_adj = input('Type an crazy adjective: ')
-
-adj_list.append(first_adj)
-adj_list.append(second_adj)
-adj_list.append(third_adj)
-adj_list.append(fourth_adj)
-adj_list.append(fifth_adj)
-adj_list.append(sixth_adj)
-
+adj_list[0] = input(colors.cyan+'Type an physical adjective: ')
+adj_list[1] = input(colors.cyan+'Type an emotional adjective: ')
+adj_list[2] = input(colors.cyan+'Type an physical adjective: ')
+adj_list[3] = input(colors.cyan+'Type an physical adjective: ')
+adj_list[4] = input(colors.cyan+'Type an emotional adjective: ')
+adj_list[5] = input(colors.cyan+'Type an crazy adjective: ')
 
 #adverbs list
-adverbs_list = []
+adverbs_list = ['adverb','adverb','adverb','adverb','adverb']
 
-first_adverb = input('Type an adverb i.e wildly: ')
-second_adverb = input ('Type an adverb: ')
-third_adverb = input ('Type an adverb: ')
-fourth_adverb = input ('Type an adverb: ')
-fifth_adverb = input ('Type an adverb: ')
+adverbs_list[0] = input(colors.purple+'Type an adverb i.e wildly: ')
+adverbs_list[1] = input (colors.purple+'Type an adverb: ')
+adverbs_list[2] = input (colors.purple+'Type an adverb: ')
+adverbs_list[3] = input (colors.purple+'Type an adverb: ')
+adverbs_list[4]= input (colors.purple+'Type an adverb: ')
 
-adverbs_list.append(first_adverb)
-adverbs_list.append(second_adverb)
-adverbs_list.append(third_adverb)
-adverbs_list.append(fourth_adverb)
-adverbs_list.append(fifth_adverb)
-
-
-print(pronouns_list[0],'of',pronouns_list[1],'was arrested this morning after they',adverbs_list[0],verb_list[0],'a',adj_list[0],nouns_list[0],'in front of some',adj_list[1],nouns_list[1],'.',pronouns_list[0],\
-'had a history of', adverbs_list[1],verb_list[1],'but no-one, not even their', nouns_list[2],pronouns_list[2],'ever imagined they would', adverbs_list[2],verb_list[2], 'with a',adj_list[2],nouns_list[3], \
-'stuck in their', nouns_list[4],'.','After eating some',nouns_list[5],', cops followed them to a',nouns_list[6],'where they reportedly',adverbs_list[3],verb_list[3],'in the fry machine .','Police commissioner',\
-pronouns_list[3], 'says that after witnessing them',adverbs_list[4],verb_list[4],'with a',adj_list[3],nouns_list[7],', there are probably going to be a lot of',adj_list[4],nouns_list[1],'that are going to need some',\
-adj_list[5],'therapy .')
-
-
-
-
-
+#Story inspired by Mad Libs book story "Weird News"
+print(colors.red +pronouns_list[0],colors.lightgrey+'of',colors.red +pronouns_list[1],colors.lightgrey+'was arrested this morning after they',colors.purple+adverbs_list[0],colors.blue+verb_list[0],colors.lightgrey+'a',colors.cyan+adj_list[0],colors.pink+nouns_list[0],colors.lightgrey+'in front of some',colors.cyan+adj_list[1],colors.pink+nouns_list[1],colors.lightgrey+'.',colors.red +pronouns_list[0],\
+colors.lightgrey+'had a history of', colors.purple+adverbs_list[1],colors.blue+verb_list[1],colors.lightgrey+'but no-one, not even \ntheir', colors.pink+nouns_list[2],colors.red +pronouns_list[2],colors.lightgrey+'ever imagined they would', colors.purple+adverbs_list[2],colors.blue+verb_list[2], colors.lightgrey+'with a',colors.cyan+adj_list[2],colors.pink+nouns_list[3], \
+colors.lightgrey+ 'stuck in their', colors.pink+nouns_list[4],colors.lightgrey+'.',colors.lightgrey+'After eating some',colors.pink+nouns_list[5],colors.lightgrey+', cops followed them to a',colors.pink+nouns_list[6],colors.lightgrey+'\nwhere they reportedly',colors.purple+adverbs_list[3],colors.blue+verb_list[3],colors.lightgrey+'in the fry machine .',colors.lightgrey+'Police commissioner',\
+colors.red +pronouns_list[3], colors.lightgrey+'says that after witnessing them',colors.purple+adverbs_list[4],colors.blue+verb_list[4],colors.lightgrey+'with a',colors.cyan+adj_list[3],colors.pink+nouns_list[7],colors.lightgrey+', \nthere are probably going to be a lot of',colors.cyan+adj_list[4],colors.pink+nouns_list[1],colors.lightgrey+'that are going to need some',\
+colors.cyan+adj_list[5],colors.lightgrey+'therapy .')
